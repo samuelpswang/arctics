ea0bc7d0d2fcce4e69de50b587173196e7576a1e
samuelpswang <samuelpswang@gmail.com>
2021-07-23 18:16:10 +0800

    Add groundwork to LogIn.js
    * Add basic text to LogIn.js

---
+++ samuelpswang/20210724-front-end/arctic-frontend/src/login/LogIn.js
@@ -0,0 +1,34 @@
+import './LogIn.css';
+import background from './img/login-background.png';
+
+
+function LogInHeader() {
+    return (
+        <h1>登入</h1>
+    );
+}
+
+function LogInForm() {
+    return (
+        <div className="LogInForm">
+            <p>帳號 (Email地址或是手機號碼)</p>
+            <p>密碼</p>
+            <p>還不是會員？按此註冊</p>
+            <p>或是</p>
+            <p>使用Google登入</p>
+            <p>使用Facebook登入</p>
+        </div>
+    );
+}
+
+function LogIn() {
+    return (
+        <>
+            <LogInHeader />
+            <LogInForm />
+            <img src={ background } alt="" />
+        </>
+    );
+}
+
+export default LogIn;
