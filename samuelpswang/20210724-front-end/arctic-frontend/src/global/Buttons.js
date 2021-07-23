010050802b348c44f0611ca0f49ca4dee7dfb8d3
samuelpswang <samuelpswang@gmail.com>
2021-07-23 18:18:55 +0800

    Add InputBox to Button.js
    * Add InputBox function to Button.js
    * InputBox placeholder = dummyText

--- samuelpswang/20210724-front-end/arctic-frontend/src/global/Buttons.js
+++ samuelpswang/20210724-front-end/arctic-frontend/src/global/Buttons.js
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 import './Buttons.css';
 
+function ActionButton(props) {
+    const[buttonName, setButtonName] = useState("buttonName");
+    
+    return (
+       <button className="">{ buttonName }</button>
+    );
+}
 
 export { InputBox, ActionButton };
