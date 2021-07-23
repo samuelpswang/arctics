0544bfa963fb374bda8b9b6f0a1f8bc0951c3561
samuelpswang <samuelpswang@gmail.com>
2021-07-23 18:20:53 +0800

    Add Button.js functionality to LogIn.js
    * Import two functions from Buttons.js
    * Replace elements in LogInForm accordingly

--- samuelpswang/20210724-front-end/arctic-frontend/src/login/LogIn.js
+++ samuelpswang/20210724-front-end/arctic-frontend/src/login/LogIn.js
@@ -1,4 +1,5 @@
 import './LogIn.css';
+import { InputBox, ActionButton } from '../global/Buttons';
 import background from './img/login-background.png';
 
 
@@ -12,8 +13,11 @@ function LogInForm() {
     return (
         <div className="LogInForm">
             <p>帳號 (Email地址或是手機號碼)</p>
+            <InputBox />
             <p>密碼</p>
+            <InputBox />
             <p>還不是會員？按此註冊</p>
+            <ActionButton />
             <p>或是</p>
             <p>使用Google登入</p>
             <p>使用Facebook登入</p>
