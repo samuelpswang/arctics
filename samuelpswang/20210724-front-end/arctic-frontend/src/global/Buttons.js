ed1f6f93ec9c5be120f57baee9eb97ee48624c3f
samuelpswang <samuelpswang@gmail.com>
2021-07-24 23:18:57 +0800

    Fix formatting in Button.js
    * Removed unused React hooks
    * Fix ActionButton formatting

--- samuelpswang/20210724-front-end/arctic-frontend/src/global/Buttons.js
+++ samuelpswang/20210724-front-end/arctic-frontend/src/global/Buttons.js
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 import './Buttons.css';
 
+
 function InputBox(props) {
-    const [dummyText] = useState("someInputText");
-    
+
     return (
         <input className="InputBox" placeholder={ props.dummyText }></input>
     );
 }
 
 function ActionButton(props) {
-    const[buttonName, setButtonName] = useState("buttonName");
     
     return (
-       <button className="ActionButton">{ props.buttonName }</button>
+        <button className="ActionButton">{ props.buttonName }</button>
+    );
+}
+
     );
 }
 
