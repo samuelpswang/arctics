98f20f7cff097a1fa0eb56df3592e5b2475bbe94
samuelpswang <samuelpswang@gmail.com>
2021-07-24 23:11:34 +0800

    Remove broken log-in button in LogIn.js

--- samuelpswang/20210724-front-end/arctic-frontend/src/login/LogIn.js
+++ samuelpswang/20210724-front-end/arctic-frontend/src/login/LogIn.js
@@ -19,7 +19,7 @@ function LogInForm() {
             <InputBox dummyText="johnnyappleseed@gmail.com" />
             <p className="Legend">密碼</p>
             <InputBox dummyText="⬤⬤⬤⬤⬤⬤⬤⬤" />
-            <p className="LogInLine"><span className="LogInText">還不是會員？<a>按此註冊</a></span><div className="LogInActionButton"><ActionButton buttonName="登入" onClick={ checkLogIn } /></div></p> 
+            <p className="LogInLine"><span className="LogInText">還不是會員？<a>按此註冊</a></span><div className="LogInActionButton"><ActionButton buttonName="登入" /></div></p> 
             <p className="OrLine"><div className="Separator"></div><span className="OrText">或是</span><div className="Separator"></div></p>
             <ExternalLoginButton buttonName="使用Google登入" iconPath={ google } altText="Google Logo" />
             <ExternalLoginButton buttonName="使用Facebook登入" iconPath={ facebook } altText="Facebook Logo" />
