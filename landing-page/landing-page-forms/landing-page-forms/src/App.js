18c3af963fed77a45280c900c58f0169fc171219
sean <>
2021-08-02 19:01:25 +0800

    fix .git problem

---
+++ landing-page/landing-page-forms/landing-page-forms/src/App.js
@@ -0,0 +1,15 @@
+import 'antd/dist/antd.css'
+import Form1 from './forms/form1'
+import Form2 from './forms/form2'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Form1 />
+      <Form2 />
+    </div>
+  );
+}
+
+export default App;
